0cfdb62b2a0d86df42bfef9e0b6f9d3f2f5477cf
Mart Emmanuel Nangit <nangitmart@gmail.com>
2026-05-20 23:13:08 +0800

    Added Currency when Purchasing Items in the Shop
    with slight issues in the ui

--- Assets/Script/PurchaseScript.cs
+++ Assets/Script/PurchaseScript.cs
@@ -10,6 +10,7 @@ public class PurchaseScript : MonoBehaviour
     public Button cookieItem;
 
     public ItemsLeft itemsLeft;
+    public PlayerCurrency playerCurrency;
 
     public void ShowPurchaseItems()
     {
@@ -28,20 +29,42 @@ public class PurchaseScript : MonoBehaviour
         Debug.Log($"[PURCHASE] Attempting to purchase: {itemName}");
 
         string cleanedItemName = itemName.Substring(4);
+        float currentCurrency = playerCurrency.GetCurrentCurrency();
+        Debug.Log($"[PURCHASE] Current currency: {currentCurrency}");
 
         if (cleanedItemName.Equals("Candy"))
         {
-            int currentCandy = itemsLeft.GetCandyLeft();
+            if (currentCurrency >= 50f)
+            {
+                int currentCandy = itemsLeft.GetCandyLeft();
 
-            itemsLeft.SetCandyLeft(currentCandy + 20);
-            Debug.Log($"[PURCHASE] Current Candy: {itemsLeft.GetCandyLeft()}");
+                itemsLeft.SetCandyLeft(currentCandy + 20);
+                Debug.Log($"[PURCHASE] Current Candy: {itemsLeft.GetCandyLeft()}");
+
+                playerCurrency.SetCurrentCurrency(currentCurrency - 50f);
+                Debug.Log($"[PURCHASE] Currency after purchase: {playerCurrency.GetCurrentCurrency()}");
+            }
+            else
+            {
+                Debug.Log("[PURCHASE] Not enough currency to purchase Candy.");
+            }
         }
         else if (cleanedItemName.Equals("Cookies"))
         {
-            int currentCookie = itemsLeft.GetCookieLeft();
+            if (currentCurrency >= 60f)
+            {
+                int currentCookie = itemsLeft.GetCookieLeft();
 
-            itemsLeft.SetCookieLeft(currentCookie + 20);
-            Debug.Log($"[PURCHASE] Current Cookies: {itemsLeft.GetCookieLeft()}");
+                itemsLeft.SetCookieLeft(currentCookie + 20);
+                Debug.Log($"[PURCHASE] Current Cookies: {itemsLeft.GetCookieLeft()}");
+
+                playerCurrency.SetCurrentCurrency(currentCurrency - 60f);
+                Debug.Log($"[PURCHASE] Currency after purchase: {playerCurrency.GetCurrentCurrency()}");
+            }
+            else
+            {
+                Debug.Log("[PURCHASE] Not enough currency to purchase Cookies.");
+            }
         }
     }
 }
